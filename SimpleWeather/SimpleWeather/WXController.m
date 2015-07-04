82177151c290e0c0ed68a51862473a439d8bc31b
Xu Yang <mobilefengxy@163.com>
2015-07-05 00:10:42 +0800

    完成tableHeaderView

--- SimpleWeather/SimpleWeather/WXController.m
+++ SimpleWeather/SimpleWeather/WXController.m
@@ -45,6 +45,88 @@
     self.tableView.separatorColor = [UIColor colorWithWhite:1 alpha:0.2];
     self.tableView.pagingEnabled = YES;
     [self.view addSubview:self.tableView];
+    
+    // Set Frame
+    CGRect headerFrame = [UIScreen mainScreen].bounds;
+    
+    CGFloat inset = 20;
+    
+    CGFloat temperatureHeight = 110;
+    CGFloat hiloHeight = 40;
+    CGFloat iconHeight = 30;
+    
+    CGRect hiloFrame = CGRectMake(inset,
+                                  (headerFrame.size.height-hiloHeight),
+                                  (headerFrame.size.width-2*inset),
+                                  hiloHeight);
+    
+    CGRect temperatureFrame = CGRectMake(inset,
+                                         (headerFrame.size.height-hiloHeight-temperatureHeight),
+                                         (headerFrame.size.width-2*inset),
+                                         temperatureHeight);
+    
+    CGRect iconFrame = CGRectMake(inset,
+                                  (headerFrame.size.height-hiloHeight-temperatureHeight-iconHeight),
+                                  iconHeight,
+                                  iconHeight);
+    
+    CGRect conditionsFrame = iconFrame;
+    conditionsFrame.origin.x += iconHeight+10;
+    conditionsFrame.size.width = headerFrame.size.width-2*inset-iconHeight;
+    
+    CGRect cityFrame = CGRectMake(0, 20, headerFrame.size.width, 30);
+    
+    // Add View
+    UIView *header = [[UIView alloc] initWithFrame:headerFrame];
+    header.backgroundColor = [UIColor clearColor];
+    self.tableView.tableHeaderView = header;
+    
+    UILabel *hiloLabel = [[UILabel alloc] initWithFrame:hiloFrame];
+    hiloLabel.backgroundColor = [UIColor clearColor];
+    hiloLabel.textColor = [UIColor whiteColor];
+    hiloLabel.text = @"0° / 0°";
+    hiloLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:28];
+    [header addSubview:hiloLabel];
+    
+    UILabel *temperatureLabel = [[UILabel alloc] initWithFrame:temperatureFrame];
+    temperatureLabel.backgroundColor = [UIColor clearColor];
+    temperatureLabel.textColor = [UIColor whiteColor];
+    temperatureLabel.text = @"0°";
+    temperatureLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:120];
+    [header addSubview:temperatureLabel];
+    
+    UIImage *iconImage = [UIImage imageNamed:@"weather-clear"];
+    UIImageView *iconView = [[UIImageView alloc] initWithFrame:iconFrame];
+    iconView.contentMode = UIViewContentModeScaleAspectFill;
+    iconView.backgroundColor = [UIColor clearColor];
+    iconView.image = iconImage;
+    [header addSubview:iconView];
+    
+    UILabel *conditionsLabel = [[UILabel alloc] initWithFrame:conditionsFrame];
+    conditionsLabel.backgroundColor = [UIColor clearColor];
+    conditionsLabel.textColor = [UIColor whiteColor];
+    conditionsLabel.text = @"clear";
+    conditionsLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:24];
+    [header addSubview:conditionsLabel];
+    
+    UILabel *cityLabel = [[UILabel alloc] initWithFrame:cityFrame];
+    cityLabel.backgroundColor = [UIColor clearColor];
+    cityLabel.textColor = [UIColor whiteColor];
+    cityLabel.text = @"Loading...";
+    cityLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:24];
+    cityLabel.textAlignment = NSTextAlignmentCenter;
+    [header addSubview:cityLabel];
+    
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
+    CGRect bounds = self.view.bounds;
+    
+    self.backgroundView.frame = bounds;
+    self.blurredImageView.frame = bounds;
+    self.tableView.frame = bounds;
 }
 
 #pragma mark - UITableViewDataSource
@@ -68,6 +150,8 @@
     cell.backgroundColor = [UIColor colorWithWhite:0 alpha:0.2];
     cell.textLabel.textColor = [UIColor whiteColor];
     cell.detailTextLabel.textColor = [UIColor whiteColor];
+    
+    //TODO:Setup the cell
     
     return cell;
 }
